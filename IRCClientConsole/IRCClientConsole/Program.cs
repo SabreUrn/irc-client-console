3ce0dfb9637ac2b7bb5dfd0b9dae7cb0cabb0b9a
SabreUrn <kasp5953@edu.easj.dk>
2018-03-23 04:20:41 +0100

    successful irc connection

---
+++ IRCClientConsole/IRCClientConsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRCClientConsole {
+	class Program {
+		static void Main(string[] args) {
+			string ip = "localhost";
+			int port = 40998;
+
+			TcpClient clientSocket = new TcpClient(ip, port);
+			Console.WriteLine("Client ready.");
+
+			using (NetworkStream ns = clientSocket.GetStream())
+			using (StreamReader sr = new StreamReader(ns))
+			using (StreamWriter sw = new StreamWriter(ns) { NewLine = "\r\n", AutoFlush = true }) {
+				//Console.CancelKeyPress += delegate {
+				//	sw.Dispose();
+				//	sr.Dispose();
+				//	ns.Dispose();
+				//	clientSocket.Dispose();
+				//};
+
+				sw.WriteLine("CAP LS 302");
+				sw.WriteLine("NICK " + "kammie");
+				sw.WriteLine("USER " + "kammie" + " 0 * " + "kammie junkie");
+				sw.WriteLine("CAP END");
+				sw.WriteLine("JOIN " + "test");
+
+				while (true) {
+					Console.WriteLine(sr.ReadLine());
+				}
+			}
+		}
+
+		private static TcpClient WaitForServer(string ip, int port) {
+			TcpClient clientSocket = new TcpClient();
+			bool serverFound = false;
+
+			while (!serverFound) {
+				try {
+					clientSocket = new TcpClient(ip, port);
+				} catch (SocketException) {
+					Console.WriteLine("Cannot find server.");
+					Console.WriteLine("Retrying in 5 seconds.");
+					System.Threading.Thread.Sleep(5000);
+				}
+			}
+
+			return clientSocket;
+		}
+	}
+}
